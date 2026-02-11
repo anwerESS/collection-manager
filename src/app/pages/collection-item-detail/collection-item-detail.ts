37c5879450d8012bd511678cacd3ae221662ef30
anwerESS <chlayfa.anouar@gmail.com>
2026-02-11 19:52:47 +0100

    Reactive Forms: (S’abonner aux changements)

--- src/app/pages/collection-item-detail/collection-item-detail.ts
+++ src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,27 +1,62 @@
-import { Component, inject, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, effect, inject, input, OnDestroy, signal } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
-import { Rarities } from '../../models/collection-item';
+import { CollectionItem, Rarities } from '../../models/collection-item';
+import { CollectionItemCard } from "../../components/collection-item-card/collection-item-card";
+import { Subscription } from 'rxjs';
+import { CollectionService } from '../../services/collection-service';
+import { Collection } from '../../models/collection';
 
 @Component({
   selector: 'app-collection-item-detail',
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CollectionItemCard],
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.css'
 })
-export class CollectionItemDetail {
+export class CollectionItemDetail implements OnDestroy {
 
   private readonly fb = inject(FormBuilder);
+  private readonly router = inject(Router);
+  private readonly collectionService = inject(CollectionService);
 
   rarities = Object.values(Rarities);
+
+  itemId = input<number | null, string | null>(null, {
+    alias: 'id',
+    transform: ((id: string | null) => id ? parseInt(id) : null)
+  });
+
+  selectedCollection!: Collection;
+  collectionItem = signal<CollectionItem>(new CollectionItem());
 
   itemFormGroup = this.fb.group({
     name: ['', [Validators.required]],
     description: ['', [Validators.required]],
     image: ['', [Validators.required]],
-    rarity: [Rarities.Common, [Validators.required]],
+    rarity: ['', [Validators.required]],
     price: [0, [Validators.required, Validators.min(0)]]
   });
+
+  valueChangeSubscription: Subscription | null = null;
+
+  constructor() {
+    effect(() => {
+      let itemToDisplay = new CollectionItem();
+      this.selectedCollection = this.collectionService.getAll()[0];
+      if (this.itemId()) {
+        const itemFound = this.selectedCollection.items.find(item => item.id === this.itemId());
+        if (itemFound) {
+          itemToDisplay = itemFound;
+        } else {
+          this.router.navigate(['not-found']);
+        }
+      }
+      this.itemFormGroup.patchValue(itemToDisplay);
+    });
+    this.valueChangeSubscription = this.itemFormGroup.valueChanges.subscribe(_ => {
+      this.collectionItem.set(Object.assign(new CollectionItem(), this.itemFormGroup.value));
+    });
+  }
 
   submit(event: Event) {
     event.preventDefault();
@@ -44,6 +79,10 @@ export class CollectionItemDetail {
         });
       };
     }
+  }
+
+  ngOnDestroy(): void {
+    this.valueChangeSubscription?.unsubscribe();
   }
 
 }
