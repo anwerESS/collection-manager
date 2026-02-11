a309fe7293a0fb5d3aa1a5661d8f7207e1045502
anwerESS <chlayfa.anouar@gmail.com>
2026-02-11 16:47:35 +0100

    Template-driven forms using `[(ngModel)]`

--- src/app/pages/collection-item-detail/collection-item-detail.ts
+++ src/app/pages/collection-item-detail/collection-item-detail.ts
@@ -1,18 +1,19 @@
-import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-collection-item-detail',
-  imports: [RouterLink],
+  imports: [FormsModule],
   templateUrl: './collection-item-detail.html',
   styleUrl: './collection-item-detail.css'
 })
 export class CollectionItemDetail {
 
-  /// id = input<string | null>(null); // doit etre id comme dans route ou definir un alias pour utliser une var avec un nom different
-  itemId = input<number | null, string | null>(null, { // signal de type number et recu de type string
-    alias: 'id',
-    transform: ((id: string | null) => id ? parseInt(id) : null)
-  });
+  name = '';
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.name);
+  }
 
 }
